4165b225197886cabfbb336cf469f11c004eb079
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-01-20 06:10:38 +0800

    Update

--- adminLogin.php
+++ adminLogin.php
@@ -5,17 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alumni Tracking System</title>
     <link rel="stylesheet" type='text/css' href="./serverSIdeCss/style.php">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div class="form-container">
-        <form action="./php/login.php" method="post" class="login">
-            <legend>Admin Login</legend>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-            <button type="submit" name="submit">Login</button>
-            <a href="./index.php" class="homepage">Back to homepage</a>
+<body class="bg-gradient-to-r from-slate-500 to-blue-500">
+    <div class="h-screen flex justify-center items-center">
+        <form action="./php/login.php" method="post" 
+        class="grid border-2 p-24 rounded-2xl">
+            <legend class="text-2xl mb-2 text-center text-4xl font-bold text-white">Admin Login</legend>
+            <label for="username" class="text-xl text-white">Username</label>
+            <input type="text" name="username" id="username"
+            class="border-2 my-2 px-2 py-1">
+            <label for="password" class="text-xl text-white">Password</label>
+            <input type="password" name="password" id="password"
+            class="border-2 my-2 px-2 py-1">
+            <button type="submit" name="submit"
+            class="w-full border-2 py-1 cursor-pointer rounded-lg font-bold mt-1 hover:bg-blue-500 text-white">Login</button>
+            <a href="./index.php" class="text-center mt-1 hover:text-blue-900 text-white">Back to homepage</a>
         </form>
     </div>
 </body>
--- index.php
+++ index.php
@@ -4,32 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alumni Tracking System</title>
-    <link rel="stylesheet" type='text/css' href="./serverSIdeCss/style.php">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div class="hero" id="hero">
-        <div class="introContainer">
-            <div class="intro">
-                <h1>Alumni Tracking System</h1>
-                <span>We're monitoring the employment status of every graduate students in Laguna State Polytechnic University</span>
-                <div class="buttons">
-                    <button class="form" id="form">Form</button>
-                    <a href="./adminLogin.php"><button class="adminLogin">Login</button></a>
-                </div>
-                <div class="formContainer" id="formContainer">
-                    <img src="./images/xWhite.png" alt="Close" id="closebtn" class="Xclose">
-                    <div class="dataIntro">
-                        <h1>Alumni Data</h1>
+<body class="bg-gradient-to-r from-slate-500 to-blue-500">
+    <div class="h-screen grid place-content-center" id="hero">
+        <div class="flex justify-center items-center h-full w-screen">
+            <div class="flex justify-center items-center w-1/2">
+                <div class="grid gap-2">
+                    <h1 class="text-4xl font-bold text-center text-white">Alumni Tracking System</h1>
+                    <span class="text-2xl text-center text-white">We're monitoring the employment status of every graduate students in Laguna State Polytechnic University</span>
+                    <div class="w-full flex justify-center items-center gap-x-10 mt-5">
+                        <button class="border-2 py-2 px-8 rounded-lg text-white hover:bg-blue-500 font-bold" id="form">Form</button>
+                        <a href="./adminLogin.php"><button class="border-2 py-2 px-8 rounded-lg hover:bg-blue-500 font-bold text-white">Login</button></a>
                     </div>
-                    <form action="./php/postData.php" method="post" class="alumniData">
-                        <div class="data">
-                        <div class="data1">
-                            <input type="text" name="fname" id="fname" placeholder="First Name">
-                            <input type="text" name="mname" id="mname" placeholder="Middle Name">
-                            <input type="text" name="lname" id="lname" placeholder="Last Name"> 
-                        </div>
-                        <div class="data3">
-                            <select name="course" id="course">
+            </div>
+                <div class=" hidden absolute border-2 border-blue-500 rounded-2xl px-20 py-10 bg-blue-500 grid place-content-center w-11/12" id="formContainer">
+                    <img src="./images/xWhite.png" alt="Close" id="closebtn" class="h-7 w-10 ml-[100%] cursor-pointer">
+                    <h1 class="text-center text-4xl font-bold text-white mb-5">Alumni Data</h1>
+                    <form action="./php/postData.php" method="post" class="">
+                        <div class="grid grid-cols-3 gap-x-3 gap-y-5">
+                            <input type="text" name="fname" id="fname" placeholder="First Name" class="h-10 px-5">
+                            <input type="text" name="mname" id="mname" placeholder="Middle Name" class="pl-5">
+                            <input type="text" name="lname" id="lname" placeholder="Last Name" class="pl-5"> 
+                            <select name="course" id="course" class="h-10 px-4">
                                 <optgroup>
                                     <option value="Bachelor of Science in Computer Science">Bachelor of Science in Computer Science</option>
                                     <option value="Bachelor of Science in Information Technology">Bachelor of Science in Information Technology</option>
@@ -39,34 +36,31 @@
                                     <option value="Bachelor of Science in Accountancy">Bachelor of Science in Accountancy</option>
                                 </optgroup>
                             </select>
-                            <select name="year" id="year">
+                            <select name="year" id="year" class="px-4">
                                 <option value="2018">2018</option>
                                 <option value="2018">2019</option>
                                 <option value="2018">2020</option>
                                 <option value="2018">2021</option>
                                 <option value="2018">2022</option>
                             </select>
-                            <input type="text" name="civilStatus" id="civilStatus" placeholder="Civil Status">
-                        </div>
-                        <div class="data2">
-                            <input type="number" name="studentNumber" id="studentNumber" placeholder="Student Number">
-                            <select name="employmentStatus" id="jobStatus">
+                            <input type="text" name="civilStatus" id="civilStatus" placeholder="Civil Status" class="px-4">
+                            <input type="number" name="studentNumber" id="studentNumber" placeholder="Student Number" class="h-10 px-4">
+                            <select name="employmentStatus" id="jobStatus" class="px-4">
                                 <option value="Employed">Employed</option>
                                 <option value="Unemployed">Unemployed</option>
                             </select>
-                            <select name="relatedOrNot" id="relatedOrNot">
+                            <select name="relatedOrNot" id="relatedOrNot" class="px-4">
                                 <option value="Related">Related</option>
                                 <option value="Not Related">Not Related</option>
                             </select>
-                        </div>  
-                        <div class="data2">
-                                <select name="gender" id="gender">
-                                    <option value="Male">Male</option>
-                                    <option value="Female">Female</option>
-                                </select>
-                            </div>    
+                            <select name="gender" id="gender" class="h-10 px-4">
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select> 
                     </div>
-                    <button type="submit" name="submit">Submit</button>
+                    <div class="grid place-content-center">
+                        <button type="submit" name="submit" class="mt-7 border-2 w-60 h-10 rounded-lg font-bold text-white hover:bg-blue-800">Submit</button>
+                    </div>
                     </form>
                 </div>
             </div>
@@ -77,15 +71,13 @@
         const formContainer = document.getElementById('formContainer');
         const closebtn = document.getElementById('closebtn');
         const body = document.body;
-
+    
         formButton.addEventListener('click', function() {
             formContainer.style.display = 'block';
-            body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
         });
-
+    
         closebtn.addEventListener('click', function() {
             formContainer.style.display = 'none';
-            body.style.backgroundColor = '';
         });
     </script>
 </body>
